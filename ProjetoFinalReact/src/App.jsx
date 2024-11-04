572d4afaa5ddab06b7345d7cd8f23d43e1d74a7c
Ana Luiza <anamotta2753@gmail.com>
2024-11-04 17:56:57 -0300

    Método post e rotas no app

--- ProjetoFinalReact/src/App.jsx
+++ ProjetoFinalReact/src/App.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import Header from './components/Header/index.jsx'
-import Footer from './components/Footer/index.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes/AppRoutes.jsx'
 
@@ -8,9 +6,7 @@ import AppRoutes from './routes/AppRoutes.jsx'
 export default function App() {
   return (
     <BrowserRouter>
-      <Header/>
       <AppRoutes/>
-      <Footer/>
     </BrowserRouter>
   )
 }
