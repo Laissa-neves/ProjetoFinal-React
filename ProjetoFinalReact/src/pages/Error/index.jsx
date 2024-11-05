1bc64bbe3661920bd293f808da0d78212d9a6444
laissa-neves <laissa.neves420@gmail.com>
2024-11-05 13:35:07 -0300

    Error alterações e css

--- ProjetoFinalReact/src/pages/Error/index.jsx
+++ ProjetoFinalReact/src/pages/Error/index.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import Header from "../../components/Header";
 import Footer from '../../components/Footer'
+import styles from './Error.module.css';
+
 
 export default function Error() {
   return (
     <div>
       <Header/>
-      <h1>Error 404 - Página não encontrada</h1>
+      <h1 className={styles.errorTitle}>Error 404 - Página não encontrada</h1>
       <Footer/>
       
     </div>
 
 
 
-  )
+  );
 }
