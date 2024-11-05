aece82c79d23e5e022c2111c95f63646668d97cd
João Guilherme Piccoli <joaoguilhermepiccoli@gmail.com>
2024-11-05 16:50:35 -0300

    Barra de busca na página buscar

--- ProjetoFinalReact/src/pages/Search/index.jsx
+++ ProjetoFinalReact/src/pages/Search/index.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "./Search.module.css";
+import * as styles from "./Search.module.css";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Search() {
   const [nome, setNome] = useState("");
@@ -8,25 +11,55 @@ export default function Search() {
   const [quantidade, serQuantidade] = useState("");
   const [preco, setPreco] = useState(0.0);
   const [produto, setProduto] = useState([]);
+  const [busca, setBusca] = useState("");
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    setBusca(e.target.value);
+  };
 
   const listaProdutos = () => {
     axios
-      .get("https://6722c0452108960b9cc577b2.mockapi.io/Produtos")
+      .get("https://6722c0692108960b9cc578da.mockapi.io/produtos")
       .then((response) => {
         setProduto(response.data);
-      });
+      })
+      .catch((error) => console.error("Erro ao buscar produtos:", error));
   };
 
   useEffect(() => {
     listaProdutos();
   }, []);
 
+  const produtosFiltrados = produto.filter((produto) =>
+    produto.nome.toLowerCase().includes(busca.toLowerCase())
+  );
+
   return (
     <div>
+      <Header />
       <div className="Busca">
         <h2>Produtos em stoque</h2>
+        <div className={styles.btnsAdd}>
+          <div className={styles.btnRoutes}>
+            <Link to={"/"}>
+              <button className={styles.btnHome}>Home</button>
+            </Link>
+          </div>
+          <div className={styles.btnRoutes}>
+            <Link to={"/buscar"}>
+              <button>Buscar</button>
+            </Link>
+          </div>
+        </div>
+        <input
+          type="text"
+          value={busca}
+          onChange={handleInputChange}
+          placeholder="Digite o nome do produto"
+        />
         <ul className="listaProduto">
-          {produto.map((produto) => (
+          {produtosFiltrados.map((produto) => (
             <li key={produto.id} className="listaItens">
               Nome: {produto.nome}
               <br />
@@ -40,6 +73,7 @@ export default function Search() {
           ))}
         </ul>
       </div>
+      <Footer />
     </div>
   );
 }
