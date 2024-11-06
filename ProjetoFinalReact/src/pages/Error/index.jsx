446abce2c1cd99fea29d9ecb3a0b39dd31f086e0
pckzin01 <patrickgoncalvesdepaiva@gmail.com>
2024-11-06 15:19:36 -0300

    Projeto Finalizado

--- ProjetoFinalReact/src/pages/Error/index.jsx
+++ ProjetoFinalReact/src/pages/Error/index.jsx
@@ -1,19 +1,20 @@
-
-import React from 'react'
+import React from "react";
 import Header from "../../components/Header";
-import Footer from '../../components/Footer'
-import styles from './Error.module.css';
-import abacate from '../../assets/abacate.png';
-
+import Footer from "../../components/Footer";
+import styles from "./Error.module.css";
+import abacate from "../../assets/abacate.png";
 
 export default function Error() {
   return (
     <div>
-      <Header/>
+      <Header />
       <h1 className={styles.errorTitle}>Error 404 - Página não encontrada</h1>
-      <img src={abacate} alt="Imagem de abacate" 
-      className={styles.abacateImage} />
-      <Footer/>
+      <img
+        src={abacate}
+        alt="Imagem de abacate"
+        className={styles.abacateImage}
+      />
+      <Footer />
     </div>
   );
 }
